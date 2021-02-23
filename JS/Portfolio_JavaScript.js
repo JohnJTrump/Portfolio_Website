681e86663082f4065a1fa1693f2a2a28963968f9
JohnJTrump <77506001+JohnJTrump@users.noreply.github.com>
2021-02-23 14:13:45 -0800

    version_2

---
+++ JS/Portfolio_JavaScript.js
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------------------------------------
+//                             JavaScript for Your Portfolio website
+//---------------------------------------------------------------------------------------------------------
+
+
+//Opens the Contact Form
+function openForm() {  
+    document.getElementById("myForm").style.display = "block";
+}
+
+//Closes contact Form
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// Controls the slide show
+var slideIndex = 1;
+    showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+        for (i =0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+        }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+
